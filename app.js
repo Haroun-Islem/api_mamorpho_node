1d3673ebfec3804f2e8c3513a56252c073dd175b
iharoun <islem.haroun@my-digital-school.org>
2023-06-21 15:37:56 +0200

    Added cors, up api and front

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 // const express = require("express");
 let express = require("express");
+let cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/auth", require("./routes/loginRoutes"));
-
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Route introuvable" });
 });
 
-// Gestion des erreurs globales
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  next();
+});
+// Gestion des erreurs globale
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ message: "Erreur serveur" });
